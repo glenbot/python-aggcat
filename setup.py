e19c7a26a87c3a21d98518d2485316f301ba8943
glenbot <glenbot@gmail.com>
2013-08-12 21:12:25 -0500

    Updated setup.py and gitignore

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ Python Aggcat
 =============
 
 Python client for Intuit Customer Account Data APIs
+
+::
+
+    pip install python-aggcat
+
+`See full documentation for quickstart <https://aggcat.readthedocs.org/en/latest/>`_
 """
 from distutils.core import setup
 __version__ = "0.1"
@@ -22,19 +28,23 @@ setup(
   download_url = 'https://github.com/glenbot/python-aggcat',
   version = __version__,
   platforms = ['Linux'],
+  dependency_links=[
+    'https://github.com/glenbot/python-oauth2/tarball/master#egg=oauth2-1.5.211-modified'
+  ],
   packages = [
     'aggcat'
   ],
   install_requires = [
     'lxml==3.2.1',
     'M2Crypto==0.21.1',
-    'requests==1.2.0'
+    'requests==1.2.0',
+    'oauth2'
   ],
   classifiers = [
     'Development Status :: 4 - Beta',
-    'Environment :: Server Environment',
+    'Environment :: Other Environment',
     'Intended Audience :: Developers',
-    'Operating System :: Linux',
+    'Operating System :: Unix',
     'Programming Language :: Python',
     'License :: OSI Approved :: MIT License'
   ]
